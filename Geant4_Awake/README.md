d8ba9084ff48e75126e06f1515f7eb9c78fac914
Gabriel Fior <gabrielfior@Gabriels-MacBook-Pro.local>
2017-04-27 02:19:29 +0200

    initial commit

---
+++ Geant4_Awake/README.md
@@ -0,0 +1,5 @@
+Geant4 geometry based almost entirely on example TestEm8 from Geant4 examples folder, available below:
+http://geant4.web.cern.ch/geant4/UserDocumentation/Doxygen/examples_doc/html/ExampleTestEm8.html
+
+# Included different quantities to be exported after each step
+# Exports csv file
--- bash_dinner.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#this is a comment-the first line sets bash as the shell script
-cd /Users/gabrielfior/data_mpp2;
-python ./process_csv_beam_v4.py /Volumes/UNTITLED/beam/10cm_1e6_400GeV_Rb10minus4_beam_cut1_5mm_nt_B4_g.csv;
-python ./process_csv_beam_v4.py /Volumes/UNTITLED/beam/10cm_1e6_400GeV_Rb10minus5_beam_cut1_5mm_nt_B4_g.csv;
-python ./process_csv_beam_v4.py /Volumes/UNTITLED/beam/10cm_1e6_400GeV_Rb10minus6_beam_cut1_5mm_nt_B4_g.csv;
-python ./process_csv_beam_v4.py /Volumes/UNTITLED/beam/10cm_1e6_400GeV_Rb10minus7_beam_cut1_5mm_nt_B4_g.csv;
-exit;
